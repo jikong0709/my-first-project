d0a78a1454902d2d6471315384c86a46a3e72881
jikong0709 <125829730+jikong0709@users.noreply.github.com>
2026-09-04 11:41:14 +0800

    Add formal app configuration

---
+++ 訂單系統app/config.js
@@ -0,0 +1,7 @@
+window.CRISP_CONFIG = Object.freeze({
+  supabaseUrl: 'https://uuefhkqtslcdkdgeyiof.supabase.co',
+  publishableKey: 'sb_publishable_v_Yzne9MJIj-9sjXYN-NDA_iA_u8wii',
+  defaultStore: 'crisp-day',
+  appName: '脆日炸雞',
+  appVersion: '2026.09.04'
+});
