e8f57c97138843f6b6a111b9d5267fe4226558a6
jikong0709 <125829730+jikong0709@users.noreply.github.com>
2026-09-04 11:52:04 +0800

    chore(order-app): bump formal app build

--- 訂單系統app/config.js
+++ 訂單系統app/config.js
@@ -3,5 +3,5 @@ window.CRISP_CONFIG = Object.freeze({
   publishableKey: 'sb_publishable_v_Yzne9MJIj-9sjXYN-NDA_iA_u8wii',
   defaultStore: 'crisp-day',
   appName: '脆日炸雞',
-  appVersion: '2026.09.04'
+  appVersion: '2026.09.04.2'
 });
